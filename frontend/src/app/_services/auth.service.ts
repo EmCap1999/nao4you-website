11d7d2e11ff7af01869a81f9f6ce6df221acaacc
Emmanuël Caputo <caputoemmanuel1999@gmail.com>
2024-07-12 18:38:28 +0200

    auth service requests and http interceptor set up

--- frontend/src/app/_services/auth.service.ts
+++ frontend/src/app/_services/auth.service.ts
@@ -1,9 +1,38 @@
+import { AUTH_API } from '../../config';
+
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  login(): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'login',
+      httpOptions
+    );
+  }
+
+  register(): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'regiser',
+      httpOptions
+    );
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'logout',
+      httpOptions
+    );
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,9 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { provideHttpClient } from '@angular/common/http';
+import { httpInterceptorProviders } from './_helpers/http.interceptor.service';
+
 
 @NgModule({
   declarations: [
@@ -18,9 +22,10 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [provideHttpClient(), httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
