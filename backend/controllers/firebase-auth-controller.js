9c9e5784a03ec67e466c00125f5f847bba739db8
Emmanuël Caputo <caputoemmanuel1999@gmail.com>
2024-07-25 20:04:35 +0200

    3 first backend github issues solved

--- backend/controllers/firebase-auth-controller.js
+++ backend/controllers/firebase-auth-controller.js
@@ -16,17 +16,14 @@ class FirebaseAuthController {
         const { email, password } = req.body;
 
         if (!email || !password) {
-            return res.status(422).json({
-                email: "Veuillez entrer une adresse mail.",
-                password: "Veuillez entrer un mot de passe.",
-            });
+            return res.status(422).json({ message: "Veuillez compléter le(s) champ(s) vide(s)" });
         }
 
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 const user = userCredential.user;
 
-                // Vérifier si l'email est vérifié
+                // Check if email is verified.
                 if (!user.emailVerified) {
                     return res.status(403).json({
                         message: "Veuillez vérifier votre adresse mail avant de vous connecter."
@@ -34,7 +31,8 @@ class FirebaseAuthController {
                 }
 
                 const idToken = userCredential._tokenResponse.idToken;
-                if (idToken) {
+
+                if (idToken != null) {
                     res.cookie('access_token', idToken, {
                         httpOnly: true
                     });
@@ -44,9 +42,17 @@ class FirebaseAuthController {
                 }
             })
             .catch((error) => {
-                console.error(error);
-                const errorMessage = error.message || "Une erreur est survenue lors de la connexion.";
-                res.status(500).json({ message: errorMessage });
+                console.error("Erreur lors de l'authentification:", error);
+
+                // Check if email verification was correctly sent.
+                if (error.code === 'auth/email-verification-failed') {
+                    res.status(500).json({
+                        error: "Problème d'envoi d'email de vérification."
+                    });
+                } else {
+                    const errorMessage = error.message || "Une erreur est survenue lors de la connexion.";
+                    res.status(500).json({ message: errorMessage });
+                }
             });
     }
 
@@ -64,18 +70,44 @@ class FirebaseAuthController {
 
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
-                sendEmailVerification(auth.currentUser)
+
+                // User created, send email verification.
+                return sendEmailVerification(auth.currentUser)
                     .then(() => {
                         res.status(201).json({ message: "Un email a été envoyé ! Veuillez vérifier votre compte." });
                     })
+                    
+                    // Handling errors related to sending the verification email
                     .catch((error) => {
-                        console.error(error);
-                        res.status(500).json({ message: "Problème d'envoi d'email de vérification." });
+                        
+                        console.error("Erreur lors de l'envoi de l'email de vérification :", error);
+                        res.status(500).json({ message: "Problème d'envoi de l'email de vérification." });
                     });
             })
+            
+            // Handling errors related to user creation
             .catch((error) => {
-                const errorMessage = error.message || "Une erreur est survenue lors de la création du nouveau membre.";
-                res.status(500).json({ message: errorMessage });
+                console.error("Erreur lors de la création de l'utilisateur :", error);
+                let errorMessage;
+
+                switch (error.code) {
+                    case 'auth/email-already-in-use':
+                        errorMessage = "Cette adresse email est déjà utilisée.";
+                        res.status(400).json({ message: errorMessage });
+                        break;
+                    case 'auth/invalid-email':
+                        errorMessage = "L'adresse email fournie n'est pas valide.";
+                        res.status(400).json({ message: errorMessage });
+                        break;
+                    case 'auth/weak-password':
+                        errorMessage = "Le mot de passe est trop faible.";
+                        res.status(400).json({ message: errorMessage });
+                        break;
+                    default:
+                        errorMessage = "Une erreur est survenue lors de la création de l'utilisateur.";
+                        res.status(500).json({ message: errorMessage });
+                        break;
+                }
             });
     }
 
