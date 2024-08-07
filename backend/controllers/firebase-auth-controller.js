70c739d6050fb9fc7202b88cfb23c258ed9ca18f
Emmanuël Caputo <caputoemmanuel1999@gmail.com>
2024-08-07 10:34:14 +0200

    ESLint (Linter) and Prettier (Formater) set up

--- backend/controllers/firebase-auth-controller.js
+++ backend/controllers/firebase-auth-controller.js
@@ -1,118 +1,133 @@
 const {
-    handleUserCreationError,
-    handleEmailVerificationError,
-    handleAuthenticationError,
-    handlePasswordResetError,
-    handleLogoutError
-} = require("../middleware/auth.error.handler");
+  handleUserCreationError,
+  handleEmailVerificationError,
+  handleAuthenticationError,
+  handlePasswordResetError,
+  handleLogoutError
+} = require('../middleware/auth.error.handler')
 
 const {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    sendEmailVerification,
-    sendPasswordResetEmail,
-    admin
-} = require('../config/firebase');
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  admin
+} = require('../config/firebase')
 
-const auth = getAuth();
+const auth = getAuth()
 
 class FirebaseAuthController {
+  loginUser = async (req, res) => {
+    const { email, password } = req.body
 
-    loginUser = async (req, res) => {
-        const { email, password } = req.body;
+    try {
+      if (!email || !password) {
+        return res
+          .status(422)
+          .json({ message: 'Veuillez compléter le(s) champ(s) vide(s)' })
+      }
 
-        try {
-            if (!email || !password) {
-                return res.status(422).json({ message: "Veuillez compléter le(s) champ(s) vide(s)" });
-            }
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      const user = userCredential.user
 
-            const userCredential = await signInWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user;
+      if (!user.emailVerified) {
+        return res.status(403).json({
+          message:
+            'Veuillez vérifier votre adresse mail avant de vous connecter.'
+        })
+      }
 
-            if (!user.emailVerified) {
-                return res.status(403).json({
-                    message: "Veuillez vérifier votre adresse mail avant de vous connecter."
-                });
-            }
+      const claims = { claims: { email: user.email } }
+      await admin.auth().setCustomUserClaims(user.uid, claims)
 
-            const claims = { claims: { email: user.email, } }
-            await admin.auth().setCustomUserClaims(user.uid, claims);
+      const newToken = await user.getIdToken()
+      res.cookie('access_token', newToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'Strict'
+      })
 
-            const newToken = await user.getIdToken();
-            res.cookie('access_token', newToken, {
-                httpOnly: true,
-                secure: true,
-                sameSite: 'Strict'
-            });
+      const decodedToken = await admin.auth().verifyIdToken(newToken)
+      req.user = decodedToken
 
-            const decodedToken = await admin.auth().verifyIdToken(newToken);
-            req.user = decodedToken;
+      return res.status(200).json({ claims: req.user.claims })
+    } catch (error) {
+      return handleAuthenticationError(error, res)
+    }
+  }
 
-            return res.status(200).json({ claims: req.user.claims });
+  // sign up
+  registerUser(req, res) {
+    const { email, password } = req.body
 
-        } catch (error) {
-            return handleAuthenticationError(error, res);
-        }
-    };
-
-
-
-    // sign up
-    registerUser(req, res) {
-        const { email, password } = req.body;
-
-        if (!email || !password) {
-            return res.status(422).json({
-                email: "Veuillez entrer une adresse mail.",
-                password: "Veuillez entrer un mot de passe.",
-            });
-        }
-
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                return sendEmailVerification(auth.currentUser)
-                    .then(() => {
-                        res.status(201).json({ message: "Un email a été envoyé ! Veuillez vérifier votre compte." });
-                    })
-                    .catch((error) => {
-                        handleEmailVerificationError(error, res);
-                    });
-            })
-            .catch((error) => {
-                handleUserCreationError(error, res);
-            });
+    if (!email || !password) {
+      return res.status(422).json({
+        email: 'Veuillez entrer une adresse mail.',
+        password: 'Veuillez entrer un mot de passe.'
+      })
     }
 
-    // sign out
-    logoutUser(req, res) {
-        signOut(auth)
-            .then(() => {
-                res.clearCookie('access_token');
-                res.status(200).json({ message: "L'utilisateur a bien été déconnecté." });
-            })
-            .catch((error) => {
-                handleLogoutError(error, res);
-            });
-    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        return sendEmailVerification(auth.currentUser)
+          .then(() => {
+            res
+              .status(201)
+              .json({
+                message:
+                  'Un email a été envoyé ! Veuillez vérifier votre compte.'
+              })
+          })
+          .catch((error) => {
+            handleEmailVerificationError(error, res)
+          })
+      })
+      .catch((error) => {
+        handleUserCreationError(error, res)
+      })
+  }
 
-    // reset password
-    resetPassword(req, res) {
-        const { email } = req.body;
-        if (!email) {
-            return res.status(422).json({
-                email: "Veuillez entrer une adresse mail valide."
-            });
-        }
-        sendPasswordResetEmail(auth, email)
-            .then(() => {
-                res.status(200).json({ message: "Un email a bien été envoyé ! Vous pouvez changer de mot de passe." });
-            })
-            .catch((error) => {
-                handlePasswordResetError(error, res);
-            });
+  // sign out
+  logoutUser(req, res) {
+    signOut(auth)
+      .then(() => {
+        res.clearCookie('access_token')
+        res
+          .status(200)
+          .json({ message: "L'utilisateur a bien été déconnecté." })
+      })
+      .catch((error) => {
+        handleLogoutError(error, res)
+      })
+  }
+
+  // reset password
+  resetPassword(req, res) {
+    const { email } = req.body
+    if (!email) {
+      return res.status(422).json({
+        email: 'Veuillez entrer une adresse mail valide.'
+      })
     }
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        res
+          .status(200)
+          .json({
+            message:
+              'Un email a bien été envoyé ! Vous pouvez changer de mot de passe.'
+          })
+      })
+      .catch((error) => {
+        handlePasswordResetError(error, res)
+      })
+  }
 }
 
-module.exports = new FirebaseAuthController();
+module.exports = new FirebaseAuthController()
--- backend/middleware/access.token.js
+++ backend/middleware/access.token.js
@@ -1,22 +1,24 @@
 // middleware/verifyToken.js
-const { admin } = require("../config/firebase");
-const { handleTokenVerificationError } = require("../middleware/auth.error.handler");
+const { admin } = require('../config/firebase')
+const {
+  handleTokenVerificationError
+} = require('../middleware/auth.error.handler')
 
 const verifyToken = async (req, res, next) => {
-    const idToken = req.cookies.access_token;
+  const idToken = req.cookies.access_token
 
-    if (!idToken) {
-        return res.status(403).json({ message: 'Utilisateur déconnecté.' });
-    }
+  if (!idToken) {
+    return res.status(403).json({ message: 'Utilisateur déconnecté.' })
+  }
 
-    try {
-        const decodedToken = await admin.auth().verifyIdToken(idToken);
-        await admin.auth().getUser(decodedToken.uid);
-        req.user = decodedToken;
-        next();
-    } catch (error) {
-        handleTokenVerificationError(error, res);
-    }
-};
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken)
+    await admin.auth().getUser(decodedToken.uid)
+    req.user = decodedToken
+    next()
+  } catch (error) {
+    handleTokenVerificationError(error, res)
+  }
+}
 
-module.exports = { verifyToken };
+module.exports = { verifyToken }
