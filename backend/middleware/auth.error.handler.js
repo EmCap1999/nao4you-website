f3fd5dfb8d4ffa1608cf235a57b90b970bf30c30
Emmanuël Caputo <caputoemmanuel1999@gmail.com>
2024-08-13 13:59:47 +0200

    PR github comments resolved (completed)

--- backend/middleware/auth.error.handler.js
+++ backend/middleware/auth.error.handler.js
@@ -1,153 +1,132 @@
-const handleAuthenticationError = (error, res) => {
-  let errorMessage
-
-  switch (error) {
-    case 'missing-item':
-      errorMessage = 'Veuillez compléter le(s) champ(s) vide(s).'
-      res.status(422).json({ message: errorMessage })
-      break
-    case 'email-not-verified':
-      errorMessage =
-        'Veuillez vérifier votre adresse mail avant de vous connecter.'
-      res.status(403).json({ message: errorMessage })
-      break
-    case 'auth/user-disabled':
-      errorMessage = 'Ce compte utilisateur a été désactivé.'
-      res.status(403).json({ message: errorMessage })
-      break
-    case 'auth/invalid-credential':
-      errorMessage = 'Adresse mail et/ou mot de passe incorrect(s).'
-      res.status(403).json({ message: errorMessage })
-      break
-    default:
-      errorMessage = 'Une erreur est survenue lors de la connexion.'
-      res.status(500).json({ message: errorMessage })
-      break
+const errorMessages = {
+  authentication: {
+    'missing-item': {
+      message: 'Veuillez compléter le(s) champ(s) vide(s).',
+      status: 422
+    },
+    'email-not-verified': {
+      message: 'Veuillez vérifier votre adresse mail avant de vous connecter.',
+      status: 403
+    },
+    'auth/user-disabled': {
+      message: 'Ce compte utilisateur a été désactivé.',
+      status: 403
+    },
+    'auth/invalid-credential': {
+      message: 'Adresse mail et/ou mot de passe incorrect(s).',
+      status: 403
+    },
+    default: {
+      message: 'Une erreur est survenue lors de la connexion.',
+      status: 500
+    }
+  },
+  userCreation: {
+    'missing-item': {
+      message: 'Veuillez compléter le(s) champ(s) vide(s).',
+      status: 422
+    },
+    'auth/email-already-in-use': {
+      message: 'Cette adresse email est déjà utilisée.',
+      status: 400
+    },
+    'auth/invalid-email': {
+      message: "L'adresse email fournie n'est pas valide.",
+      status: 400
+    },
+    'auth/weak-password': {
+      message: 'Le mot de passe est trop faible.',
+      status: 400
+    },
+    default: {
+      message: "Une erreur est survenue lors de la création de l'utilisateur.",
+      status: 500
+    }
+  },
+  emailVerification: {
+    'auth/invalid-email': {
+      message: "L'adresse email fournie n'est pas valide.",
+      status: 400
+    },
+    'auth/too-many-requests': {
+      message: 'Trop de requêtes envoyées. Veuillez réessayer plus tard.',
+      status: 429
+    },
+    default: {
+      message: "Problème d'envoi de l'email de vérification.",
+      status: 500
+    }
+  },
+  passwordReset: {
+    'missing-item': {
+      message: 'Veuillez entrer une adresse mail.',
+      status: 422
+    },
+    'auth/user-not-found': { message: 'Utilisateur non trouvé.', status: 404 },
+    'auth/invalid-email': {
+      message: "L'adresse email fournie n'est pas valide.",
+      status: 400
+    },
+    'auth/too-many-requests': {
+      message: 'Trop de requêtes envoyées. Veuillez réessayer plus tard.',
+      status: 429
+    },
+    default: {
+      message:
+        'Une erreur est survenue lors de la réinitialisation du mot de passe.',
+      status: 500
+    }
+  },
+  logout: {
+    default: { message: 'Erreur de serveur interne.', status: 500 }
+  },
+  tokenVerification: {
+    'user-disconnected': {
+      message: "L'utilisateur est déconnecté.",
+      status: 403
+    },
+    'auth/argument-error': {
+      message: 'Le format du token est invalide.',
+      status: 400
+    },
+    'auth/user-not-found': {
+      message: 'Utilisateur non trouvé ou supprimé.',
+      status: 403
+    },
+    'auth/id-token-expired': { message: 'Le token a expiré.', status: 401 },
+    'auth/id-token-revoked': {
+      message: 'Le token a été révoqué.',
+      status: 401
+    },
+    'auth/invalid-id-token': {
+      message: 'Le token fourni est invalide.',
+      status: 403
+    },
+    'error-creation': {
+      message: 'Erreur de création du token personnalisé.',
+      status: 403
+    },
+    default: { message: 'Non-autorisé.', status: 403 }
   }
 }
 
-const handleUserCreationError = (error, res) => {
-  console.error("Erreur lors de la création de l'utilisateur :", error)
-  let errorMessage
-
-  switch (error) {
-    case 'missing-item':
-      errorMessage = 'Veuillez compléter le(s) champ(s) vide(s).'
-      res.status(422).json({ message: errorMessage })
-      break
-    case 'auth/email-already-in-use':
-      errorMessage = 'Cette adresse email est déjà utilisée.'
-      res.status(400).json({ message: errorMessage })
-      break
-    case 'auth/invalid-email':
-      errorMessage = "L'adresse email fournie n'est pas valide."
-      res.status(400).json({ message: errorMessage })
-      break
-    case 'auth/weak-password':
-      errorMessage = 'Le mot de passe est trop faible.'
-      res.status(400).json({ message: errorMessage })
-      break
-    default:
-      errorMessage =
-        "Une erreur est survenue lors de la création de l'utilisateur."
-      res.status(500).json({ message: errorMessage })
-      break
-  }
+const handleError = (type, error, res) => {
+  const errorType = errorMessages[type] || errorMessages.default
+  const { message, status } = errorType[error] || errorType.default
+  res.status(status).json({ message })
 }
 
-const handleEmailVerificationError = (error, res) => {
-  console.log(error)
-  let errorMessage
-
-  switch (error) {
-    case 'auth/invalid-email':
-      errorMessage = "L'adresse email fournie n'est pas valide."
-      res.status(400).json({ message: errorMessage })
-      break
-    case 'auth/too-many-requests':
-      errorMessage = 'Trop de requêtes envoyées. Veuillez réessayer plus tard.'
-      res.status(429).json({ message: errorMessage })
-      break
-    default:
-      errorMessage = "Problème d'envoi de l'email de vérification."
-      res.status(500).json({ message: errorMessage })
-      break
-  }
-}
-
-const handlePasswordResetError = (error, res) => {
-  console.log(error)
-  let errorMessage
-
-  switch (error) {
-    case 'missing-item':
-      errorMessage = 'Veuillez entrer une adresse mail.'
-      res.status(422).json({ message: errorMessage })
-      break
-    case 'auth/user-not-found':
-      errorMessage = 'Utilisateur non trouvé.'
-      res.status(404).json({ message: errorMessage })
-      break
-    case 'auth/invalid-email':
-      errorMessage = "L'adresse email fournie n'est pas valide."
-      res.status(400).json({ message: errorMessage })
-      break
-    case 'auth/too-many-requests':
-      errorMessage = 'Trop de requêtes envoyées. Veuillez réessayer plus tard.'
-      res.status(429).json({ message: errorMessage })
-      break
-    default:
-      errorMessage =
-        'Une erreur est survenue lors de la réinitialisation du mot de passe.'
-      res.status(500).json({ message: errorMessage })
-      break
-  }
-}
-
-const handleLogoutError = (error, res) => {
-  console.log(error)
-  res.status(500).json({ message: 'Erreur de serveur interne.' })
-}
-
-const handleTokenVerificationError = (error, res) => {
-  console.log(error)
-  let errorMessage
-
-  switch (error) {
-    case 'user-disconnected':
-      errorMessage = "L'utilisateur est déconnecté."
-      res.status(403).json({ message: errorMessage })
-      break
-    case 'auth/argument-error':
-      errorMessage = 'Le format du token est invalide.'
-      res.status(400).json({ message: errorMessage })
-      break
-    case 'auth/user-not-found':
-      errorMessage = 'Utilisateur non trouvé ou supprimé.'
-      res.status(403).json({ message: errorMessage })
-      break
-    case 'auth/id-token-expired':
-      errorMessage = 'Le token a expiré.'
-      res.status(401).json({ message: errorMessage })
-      break
-    case 'auth/id-token-revoked':
-      errorMessage = 'Le token a été révoqué.'
-      res.status(401).json({ message: errorMessage })
-      break
-    case 'auth/invalid-id-token':
-      errorMessage = 'Le token fourni est invalide.'
-      res.status(403).json({ message: errorMessage })
-      break
-    case 'error-creation':
-      errorMessage = 'Erreur de création du token personnalisé.'
-      res.status(403).json({ message: errorMessage })
-      break
-    default:
-      errorMessage = 'Non-autorisé.'
-      res.status(403).json({ message: errorMessage })
-      break
-  }
-}
+const handleAuthenticationError = (error, res) =>
+  handleError('authentication', error, res)
+const handleUserCreationError = (error, res) =>
+  handleError('userCreation', error, res)
+const handleEmailVerificationError = (error, res) =>
+  handleError('emailVerification', error, res)
+const handlePasswordResetError = (error, res) =>
+  handleError('passwordReset', error, res)
+const handleLogoutError = (error, res) => handleError('logout', error, res)
+const handleTokenVerificationError = (error, res) =>
+  handleError('tokenVerification', error, res)
 
 module.exports = {
   handleAuthenticationError,
