763ce1c7223e80be8a5bb40087f7658fbdcf40eb
Emmanuël Caputo <136804334+EmCap1999@users.noreply.github.com>
2025-04-04 19:24:42 +0200

    Test branch (#11)
    * add workflow on code for CI-CD biome checks
    
    * add workflow on code for CI-CD biome checks
    
    * add workflow on code for CI-CD biome checks
    
    * add workflow on code for CI-CD biome checks
    
    * add workflow on code for CI-CD biome checks
    
    * add workflow on code for CI-CD biome checks
    
    * add workflow on code for CI-CD biome checks
    
    * add workflow on code for CI-CD biome checks

--- backend/src/controllers/firebase.auth.controller.js
+++ backend/src/controllers/firebase.auth.controller.js
@@ -1,109 +1,109 @@
 const {
-	getAuth,
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
-	signOut,
-	sendEmailVerification,
-	sendPasswordResetEmail,
-} = require("../configs/firebase.auth.config");
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+} = require('../configs/firebase.auth.config');
 
 const {
-	getFirebaseErrorMessage,
-	validateRequiredFields,
-} = require("../errors/firebase.errors");
+  getFirebaseErrorMessage,
+  validateRequiredFields,
+} = require('../errors/firebase.errors');
 
 const auth = getAuth();
 
 class FirebaseAuthController {
-	async SignUp(req, res) {
-		const missingFields = validateRequiredFields(req.body);
-		if (missingFields.length > 0) {
-			return res.status(400).json({ errors: missingFields });
-		}
+  async SignUp(req, res) {
+    const missingFields = validateRequiredFields(req.body);
+    if (missingFields.length > 0) {
+      return res.status(400).json({ errors: missingFields });
+    }
 
-		try {
-			const { email, password } = req.body;
+    try {
+      const { email, password } = req.body;
 
-			const userCredential = await createUserWithEmailAndPassword(
-				auth,
-				email,
-				password,
-			);
-			await sendEmailVerification(userCredential.user);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      await sendEmailVerification(userCredential.user);
 
-			res
-				.status(201)
-				.json({ message: "Un email de vérification a été envoyé !" });
-		} catch (error) {
-			const errorMessage = getFirebaseErrorMessage(error.code);
-			return res.status(500).json({ error: errorMessage });
-		}
-	}
+      res
+        .status(201)
+        .json({ message: 'Un email de vérification a été envoyé !' });
+    } catch (error) {
+      const errorMessage = getFirebaseErrorMessage(error.code);
+      return res.status(500).json({ error: errorMessage });
+    }
+  }
 
-	async SignIn(req, res) {
-		const missingFields = validateRequiredFields(req.body);
-		if (missingFields.length > 0) {
-			return res.status(400).json({ errors: missingFields });
-		}
+  async SignIn(req, res) {
+    const missingFields = validateRequiredFields(req.body);
+    if (missingFields.length > 0) {
+      return res.status(400).json({ errors: missingFields });
+    }
 
-		try {
-			const { email, password } = req.body;
+    try {
+      const { email, password } = req.body;
 
-			const userCredential = await signInWithEmailAndPassword(
-				auth,
-				email,
-				password,
-			);
-			const user = userCredential.user;
-			const idToken = await user.getIdToken();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      const idToken = await user.getIdToken();
 
-			if (idToken) {
-				res.cookie("access_token", idToken, {
-					httpOnly: true,
-					secure: true,
-					sameSite: "strict",
-				});
-				return res
-					.status(200)
-					.json({ message: `L'utilisateur ${user.email} est connecté.` });
-			}
-			const errorMessage = "Token d'authentification manquant.";
-			return res.status(500).json({ error: errorMessage });
-		} catch (error) {
-			const errorMessage = getFirebaseErrorMessage(error.code);
-			return res.status(500).json({ error: errorMessage });
-		}
-	}
+      if (idToken) {
+        res.cookie('access_token', idToken, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'strict',
+        });
+        return res
+          .status(200)
+          .json({ message: `L'utilisateur ${user.email} est connecté.` });
+      }
+      const errorMessage = "Token d'authentification manquant.";
+      return res.status(500).json({ error: errorMessage });
+    } catch (error) {
+      const errorMessage = getFirebaseErrorMessage(error.code);
+      return res.status(500).json({ error: errorMessage });
+    }
+  }
 
-	async SignOut(req, res) {
-		try {
-			await signOut(auth);
-			res.clearCookie("access_token");
-			res.status(200).json({ message: "Utilisateur déconnecté avec succès." });
-		} catch (error) {
-			const errorMessage = getFirebaseErrorMessage(error.code);
-			return res.status(500).json({ error: errorMessage });
-		}
-	}
+  async SignOut(req, res) {
+    try {
+      await signOut(auth);
+      res.clearCookie('access_token');
+      res.status(200).json({ message: 'Utilisateur déconnecté avec succès.' });
+    } catch (error) {
+      const errorMessage = getFirebaseErrorMessage(error.code);
+      return res.status(500).json({ error: errorMessage });
+    }
+  }
 
-	async ResetPassword(req, res) {
-		const missingFields = validateRequiredFields(req.body);
-		if (missingFields.length > 0) {
-			return res.status(400).json({ errors: missingFields });
-		}
+  async ResetPassword(req, res) {
+    const missingFields = validateRequiredFields(req.body);
+    if (missingFields.length > 0) {
+      return res.status(400).json({ errors: missingFields });
+    }
 
-		try {
-			const { email } = req.body;
+    try {
+      const { email } = req.body;
 
-			await sendPasswordResetEmail(auth, email);
-			res.status(200).json({
-				message: "Email envoyé avec succès pour changer de mot de passe.",
-			});
-		} catch (error) {
-			const errorMessage = getFirebaseErrorMessage(error.code);
-			return res.status(500).json({ error: errorMessage });
-		}
-	}
+      await sendPasswordResetEmail(auth, email);
+      res.status(200).json({
+        message: 'Email envoyé avec succès pour changer de mot de passe.',
+      });
+    } catch (error) {
+      const errorMessage = getFirebaseErrorMessage(error.code);
+      return res.status(500).json({ error: errorMessage });
+    }
+  }
 }
 
 module.exports = new FirebaseAuthController();
--- backend/src/errors/firebase.errors.js
+++ backend/src/errors/firebase.errors.js
@@ -1,56 +1,56 @@
 function getFirebaseErrorMessage(errorCode) {
-	switch (errorCode) {
-		// sign in-up and reset password errors
-		case "auth/missing-email":
-			return "L'adresse e-mail est manquante.";
-		case "auth/email-already-in-use":
-			return "L'adresse e-mail est déjà utilisée par un autre compte.";
-		case "auth/invalid-email":
-			return "L'adresse e-mail n'est pas valide.";
-		case "auth/weak-password":
-			return "Le mot de passe est trop faible.";
-		case "auth/user-not-found":
-			return "Aucun utilisateur trouvé avec cette adresse e-mail.";
-		case "auth/invalid-credential":
-			return "Information(s) d'identification invalide(s).";
-		case "auth/wrong-password":
-			return "Le mot de passe est incorrect.";
+  switch (errorCode) {
+    // sign in-up and reset password errors
+    case 'auth/missing-email':
+      return "L'adresse e-mail est manquante.";
+    case 'auth/email-already-in-use':
+      return "L'adresse e-mail est déjà utilisée par un autre compte.";
+    case 'auth/invalid-email':
+      return "L'adresse e-mail n'est pas valide.";
+    case 'auth/weak-password':
+      return 'Le mot de passe est trop faible.';
+    case 'auth/user-not-found':
+      return 'Aucun utilisateur trouvé avec cette adresse e-mail.';
+    case 'auth/invalid-credential':
+      return "Information(s) d'identification invalide(s).";
+    case 'auth/wrong-password':
+      return 'Le mot de passe est incorrect.';
 
-		// token errors
-		case "auth/invalid-custom-token":
-			return "Le token vérifié est invalide.";
-		case "auth/custom-token-mismatch":
-			return "Le token vérifié ne correspond pas.";
-		case "auth/invalid-id-token":
-			return "Le token ID vérifié est invalide.";
-		case "auth/id-token-expired":
-			return "Le token ID vérifié a expiré.";
-		case "auth/id-token-revoked":
-			return "Le token ID vérifié a été révoqué.";
-		case "auth/session-cookie-expired":
-			return "Le cookie de session vérifié a expiré.";
-		case "auth/session-cookie-revoked":
-			return "Le cookie de session vérifié a été révoqué.";
+    // token errors
+    case 'auth/invalid-custom-token':
+      return 'Le token vérifié est invalide.';
+    case 'auth/custom-token-mismatch':
+      return 'Le token vérifié ne correspond pas.';
+    case 'auth/invalid-id-token':
+      return 'Le token ID vérifié est invalide.';
+    case 'auth/id-token-expired':
+      return 'Le token ID vérifié a expiré.';
+    case 'auth/id-token-revoked':
+      return 'Le token ID vérifié a été révoqué.';
+    case 'auth/session-cookie-expired':
+      return 'Le cookie de session vérifié a expiré.';
+    case 'auth/session-cookie-revoked':
+      return 'Le cookie de session vérifié a été révoqué.';
 
-		// sign out errors
-		case "auth/no-current-user":
-			return "Aucun utilisateur n'est actuellement connecté.";
+    // sign out errors
+    case 'auth/no-current-user':
+      return "Aucun utilisateur n'est actuellement connecté.";
 
-		default:
-			console.error(`Unhandled error code: ${errorCode}`);
-			return "Erreur interne du serveur";
-	}
+    default:
+      console.error(`Unhandled error code: ${errorCode}`);
+      return 'Erreur interne du serveur';
+  }
 }
 
 function validateRequiredFields(body) {
-	return Object.entries(body)
-		.filter(([key, value]) => {
-			return value === undefined || value === null || value === "";
-		})
-		.map(([key]) => `Le champ ${key} est requis.`);
+  return Object.entries(body)
+    .filter(([key, value]) => {
+      return value === undefined || value === null || value === '';
+    })
+    .map(([key]) => `Le champ ${key} est requis.`);
 }
 
 module.exports = {
-	getFirebaseErrorMessage,
-	validateRequiredFields,
+  getFirebaseErrorMessage,
+  validateRequiredFields,
 };
