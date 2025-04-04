0e5ef2e4ac0e9dd7e68dde67c81e5711c21cccfb
Manu Caputo <emmanuel.caputo@necko.tech>
2025-04-04 19:13:27 +0200

    add workflow on code for CI-CD biome checks

--- backend/src/errors/server.errors.js
+++ backend/src/errors/server.errors.js
@@ -1,18 +1,18 @@
 function getServerErrorMessage(errorCode) {
-	switch (errorCode) {
-		case "EACCES":
-			return "Permission refusée. Veuillez vérifier vos droits d'accès.";
-		case "EADDRINUSE":
-			return "Adresse déjà utilisée. Veuillez vérifier si le port est déjà utilisé.";
-		case "ENOTFOUND":
-			return "Serveur non trouvé. Veuillez vérifier la configuration de votre serveur.";
-		case "ECONNREFUSED":
-			return "Connexion refusée. Veuillez vérifier la connexion de votre serveur.";
+  switch (errorCode) {
+    case 'EACCES':
+      return "Permission refusée. Veuillez vérifier vos droits d'accès.";
+    case 'EADDRINUSE':
+      return 'Adresse déjà utilisée. Veuillez vérifier si le port est déjà utilisé.';
+    case 'ENOTFOUND':
+      return 'Serveur non trouvé. Veuillez vérifier la configuration de votre serveur.';
+    case 'ECONNREFUSED':
+      return 'Connexion refusée. Veuillez vérifier la connexion de votre serveur.';
 
-		default:
-			console.error(`Unknown server error code: ${errorCode}`);
-			return "Une erreur inconnue s'est produite lors du démarrage du serveur.";
-	}
+    default:
+      console.error(`Unknown server error code: ${errorCode}`);
+      return "Une erreur inconnue s'est produite lors du démarrage du serveur.";
+  }
 }
 
 module.exports = { getServerErrorMessage };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,38 +1,38 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-require("dotenv").config();
-const router = require("./routes/firebase.auth.routes");
-const { getServerErrorMessage } = require("./errors/server.errors");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const router = require('./routes/firebase.auth.routes');
+const { getServerErrorMessage } = require('./errors/server.errors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 async function startServer() {
-	if (!process.env.PORT) {
-		console.error("❌ Error: Please define the PORT in your .env file.");
-		process.exit(1);
-	}
+  if (!process.env.PORT) {
+    console.error('❌ Error: Please define the PORT in your .env file.');
+    process.exit(1);
+  }
 
-	try {
-		app.use(express.json());
-		app.use(cookieParser());
-		app.use(router);
+  try {
+    app.use(express.json());
+    app.use(cookieParser());
+    app.use(router);
 
-		app.listen(PORT, () => {
-			console.log("✅ Node.js server is running.");
-		});
-	} catch (error) {
-		console.error("❌ Node Server startup error:", error);
-		console.error(getServerErrorMessage(error.code));
-		process.exit(1);
-	}
+    app.listen(PORT, () => {
+      console.log('✅ Node.js server is running.');
+    });
+  } catch (error) {
+    console.error('❌ Node Server startup error:', error);
+    console.error(getServerErrorMessage(error.code));
+    process.exit(1);
+  }
 }
 
 (async () => {
-	try {
-		await startServer();
-	} catch (error) {
-		console.error("❌ Failed to start the server:", error);
-		process.exit(1);
-	}
+  try {
+    await startServer();
+  } catch (error) {
+    console.error('❌ Failed to start the server:', error);
+    process.exit(1);
+  }
 })();
