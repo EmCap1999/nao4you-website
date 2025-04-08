0d4f81faa963fcb30b0150ce3190dadb03e96b78
Manu Caputo <emmanuel.caputo@necko.tech>
2025-04-08 21:04:17 +0200

    fix(routes): delete token verification route after testing

--- backend/src/routes/firebase.auth.routes.js
+++ backend/src/routes/firebase.auth.routes.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const firebaseAuthController = require('../controllers/firebase.auth.controller')
-const verifyToken = require('../middlewares/firebase.auth.middleware')
-
-router.get('/protected', verifyToken, (req, res) => {
-  res.status(200).json({ message: 'Accès autorisé', user: req.user })
-})
 
 router.post('/sign-up', firebaseAuthController.SignUp)
 router.post('/sign-in', firebaseAuthController.SignIn)
